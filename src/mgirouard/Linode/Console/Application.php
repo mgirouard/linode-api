15e00a64ea80eb917638381ba0eab593f139a3a6
Michael Girouard <mgirouard@pushhere.com>
2014-10-08 01:23:44 -0400

    Dynamically inject a callable into the command

--- src/mgirouard/Linode/Console/Application.php
+++ src/mgirouard/Linode/Console/Application.php
@@ -2,6 +2,7 @@
 
 namespace mgirouard\Linode\Console;
 
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,13 +13,27 @@ use Symfony\Component\Console\Formatter\OutputFormatter;
 
 class Application extends BaseApplication
 {
-    public function __construct($spec)
+    public function __construct($spec, Client $client)
     {
         if (empty($spec->DATA->METHODS)) throw new \InvalidArgumentException('Bad spec supplied');
 
         foreach ($spec->DATA->METHODS as $name => $method) {
             $command = new Command(str_replace('.', ':', $name));
             $command->setDescription($method->DESCRIPTION);
+
+            foreach ($method->PARAMETERS as $param) {
+                $command->addOption(
+                    $param->NAME,
+                    null,
+                    InputOption::VALUE_REQUIRED,
+                    $param->DESCRIPTION
+                );
+            }
+
+            $command->setCode(function (InputInterface $input, OutputInterface $output) use ($client) {
+                $output->writeln('It works!');
+            });
+
             $this->add($command);
         }
 
